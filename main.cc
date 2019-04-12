98ce6f6ba86d823d2400a97a95e090e38edd605d
Shrinish <shdonde@gmail.com>
2019-04-12 15:29:07 +0200

    interesting paper on super-optimisation

--- main.cc
+++ main.cc
@@ -1,13 +1,13 @@
 #include <cstdlib>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 using namespace std;
 
-size_t popcnt(uint64_t x) 
-{
+size_t popcnt(uint64_t x) {
   int res = 0;
-  for ( ; x > 0; x >>= 1 ) {
+  for (; x > 0; x >>= 1) {
     res += x & 0x1ull;
   }
   return res;
@@ -15,11 +15,22 @@ size_t popcnt(uint64_t x)
 
 int main(int argc, char** argv) {
   const auto itr = atoi(argv[1]);
-  auto ret = 0;
-  for ( auto i = 0; i < itr; ++i ) {
-    
-    ret += popcnt(i);
-  
-  }
-  return ret;
 
+  auto ret = 0;
+  uint64_t j = 1;
+  for (auto i = 0; i < itr; ++i) {
+
+    // a few considerations: the multiplier (currently 19) should not be a
+    // power of 2, lest this becomes a bit-shift and we only get *much* less
+    // variety.  We don't want to just add rand() because that could be as high
+    // as RAND_MAX.  We don't want to leave out rand because that would make
+    // the testcases deterministic and all 7 modulo 19 (at least until it wraps
+    // around the 64-bit limit).
+
+    j = (j*19 + 7 + (rand() % 5));
+    ret += popcnt(j);
+  }
+
+  return ret;
+}
+       
