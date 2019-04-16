0232a906fe7d6ec5a36051a60fd0f6715b89d4af
Shrinish <shdonde@gmail.com>
2019-04-16 12:27:01 +0200

    std libc for wcpcpy

---
+++ wcpcpy_target.s
@@ -0,0 +1,19 @@
+#target                       #rewrite
+.begin :                      movl esi , esi
+movl esi , esi                movl ( r15 , rsi ) , edx
+movl ( r15 , rsi ) , edx      addl 4 , esi
+movl edi , eax                nop (23)
+addl 4 , esi                  .begin :
+movl edi , edi                movl edi, eax
+movl edx , ( r15 , rdi )      movl edi , edi
+addl 4 , edi                  movl ( r15 , rsi ) , edx
+testl edx , edx               shrl 1 , edx
+jne . begin                   je . exit             
+retq                          movl esi , esi
+							  movl ( r15 , rsi ) , edx
+							  addl 4 , esi
+							  jmpq . begin
+							  nop (31)
+							  . exit :
+							  retq
+
