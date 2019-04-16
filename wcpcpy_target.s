63666d29ee1f3adc9350c40b12f4c423b0ea4a9b
Shrinish Donde <shdonde@gmail.com>
2019-04-16 12:28:52 +0200

    Fix whitespace

--- wcpcpy_target.s
+++ wcpcpy_target.s
@@ -10,10 +10,10 @@ addl 4 , edi                  movl ( r15 , rsi ) , edx
 testl edx , edx               shrl 1 , edx
 jne . begin                   je . exit             
 retq                          movl esi , esi
-							  movl ( r15 , rsi ) , edx
-							  addl 4 , esi
-							  jmpq . begin
-							  nop (31)
-							  . exit :
-							  retq
+			      movl ( r15 , rsi ) , edx
+                   	      addl 4 , esi
+			      jmpq . begin
+			      nop (31)
+			      . exit :
+			      retq
 
