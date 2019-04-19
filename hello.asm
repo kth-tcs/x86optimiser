49bb63027c8c9ee4ac70214df667636355704705
Shrinish <shdonde@gmail.com>
2019-04-19 11:42:49 +0200

    hello.asm

--- hello.asm
+++ hello.asm
@@ -14,10 +14,11 @@ len:	equ $-msg		; "$" means "here"
         global main		; make label available to linker 
 main:				; standard  gcc  entry point
 	
-	mov	edx,len		; arg3, length of string to print
-	mov	ecx,msg		; arg2, pointer to string
-	mov	ebx,1		; arg1, where to write, screen
-	mov	eax,4		; write sysout command to int 80 hex
+	mov	edx,len      ; arg3, length of string to print
+	mov	ecx,msg	; arg2, pointer to string
+	mov	ebx,0x80	; arg1, where to write, screen
+	mov	eax,0x81	; write sysout command to int 80 hex
+       xor   ebx, eax
 	int	0x80		; interrupt 80 hex, call kernel
 	
 	mov	ebx,0		; exit code, 0=normal
