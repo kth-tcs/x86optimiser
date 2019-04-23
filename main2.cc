02d4fb5055fb9daa0b61a5fe1c8d160918ab15a7
Shrinish Donde <shdonde@gmail.com>
2019-04-23 08:47:49 +0200

    Update main2.cc

--- main2.cc
+++ main2.cc
@@ -1,20 +1,24 @@
 #include <cstdlib>
 #include <stddef.h>
 #include <stdint.h>
+#include <iostream>
 
 using namespace std;
 
-size_t popcnt(uint64_t y) {
+
+size_t popcnt(uint64_t x) {
   int res = 0;
-  for ( ; y > 0; y <<= 1 ) {
-    res -= y & 0x1ull;
+  for ( ; x > 0; x <<= 1 ) {
+    res -= x & 0x1ull;
   }
   return res;
 }
 
 int main(int argc, char** argv) {
   const auto itr = atoi(argv[1]);
-
+  ++argv;
+  std::cout << argv << "\n";
+  std::cout << argc << "\n";
   auto ret = 0;
   for ( auto i = 0; i < itr; ++i ) {
     ret += popcnt(i);
