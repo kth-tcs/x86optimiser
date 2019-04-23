5c7d90448a6bb128d4bacd7ce07f36da8c8baa5f
Shrinish Donde <shdonde@gmail.com>
2019-04-23 08:46:05 +0200

    Create _GLOBAL__sub_I__Z6popcntm.s
    Self generated

---
+++ _GLOBAL__sub_I__Z6popcntm.s
@@ -0,0 +1,14 @@
+#cat _GLOBAL__sub_I__Z6popcntm.s 
+  .text
+  .globl _GLOBAL__sub_I__Z6popcntm
+  .type _GLOBAL__sub_I__Z6popcntm, @function
+
+#! file-offset 0x820
+#! rip-offset  0x400820
+#! capacity    5 bytes
+
+# Text                                                               #  Line  RIP       Bytes  Opcode            
+._GLOBAL__sub_I__Z6popcntm:                                          #        0x400820  0      OPC=<label>       
+  jmpq ._Z41__static_initialization_and_destruction_0ii_constprop_1  #  1     0x400820  5      OPC=jmpq_label_1  
+                                                                                                                 
+.size _GLOBAL__sub_I__Z6popcntm, .-_GLOBAL__sub_I__Z6popcntm
